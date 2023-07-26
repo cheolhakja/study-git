96b2c4691403f0b111a76de6d348f231c50d1553
cheolhakja <rebegin15@gmail.com>
2023-07-26 18:15:01 +0900

    asyncio 네이티브 코루틴으로 덧셈작업해봄

--- test.py
+++ test.py
@@ -11,3 +11,22 @@ print("ver2")
 print("use git clone for working with another computer")
 
 print("2nd computer(directory)  add something  to   test git pull")
+
+import asyncio
+import time
+
+async def add(a, b):
+    print("1과 2의 덧셈을 계산하고 결과를 반환하는 함수입니다")
+    await asyncio.sleep(10)
+    return a+b
+
+async def print_add(a, b):
+    result = await add(a, b)    
+    print('print_add: {0} + {1} = {2}'.format(a, b, result))
+
+now = time.time()
+loop = asyncio.get_event_loop()           
+loop.run_until_complete(print_add(1, 2))    
+loop.close()   
+end = time.time()
+print(f"시작은 {now}이고 끝은 {end}입니다")
